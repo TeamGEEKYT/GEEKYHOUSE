01324e48fb5877c71dd04fc1698c50d4d38f0a78
TeamGEEKYT <142260289+TeamGEEKYT@users.noreply.github.com>
2026-03-01 03:30:41 +0100

    Add files via upload

---
+++ Website/i18n.js
@@ -0,0 +1,30 @@
+const i18n = {
+  currentLang: 'fr',
+  translations: {},
+
+  async load(lang) {
+    const res = await fetch(`lang_${lang}.json`);
+    this.translations[lang] = await res.json();
+  },
+
+  t(key) {
+    return this.translations[this.currentLang]?.[key]
+        ?? key;
+  },
+
+  apply() {
+    document.querySelectorAll('[data-i18n]')
+      .forEach(el => {
+        el.innerHTML = this.t(el.dataset.i18n);
+      });
+    document.documentElement.lang = this.currentLang;
+  },
+
+  async setLang(lang) {
+    if (!this.translations[lang])
+      await this.load(lang);
+    this.currentLang = lang;
+    this.apply();
+    localStorage.setItem('lang', lang);
+  }
+};
